936b03f5129e272600ab546622b37b18199de74b
zyayoung <1328410180@qq.com>
2020-01-04 15:26:52 +0800

    Update slave

--- NPU/exp_slave.py
+++ NPU/exp_slave.py
@@ -1,3 +1,20 @@
+############################
+# Configurations
+############################
+
+SSID        = "TP-LINK_AB74"
+PASSWORD    = "young12345"
+MASTER_IP   = "192.168.0.112"
+DEVICE_NAME = "001"
+
+import lcd
+FONT_COLOT  = lcd.WHITE
+
+
+############################
+# Import
+############################
+
 from fpioa_manager import *
 import machine
 
@@ -10,42 +27,134 @@ import os
 import socket
 import network
 import gc
+import uerrno
+import sys
 
-SSID        = "Mouse"
-PASSWORD    = "12345678"
-MASTER_IP  = "192.168.43.94"
-DEVICE_NAME = "001"
+def clip(x, a, b):
+    if x<a: x=a
+    if x>b: x=b
+    return x
 
-fm.register(board_info.WIFI_RX,fm.fpioa.UART2_TX)
-fm.register(board_info.WIFI_TX,fm.fpioa.UART2_RX)
-uart = machine.UART(machine.UART.UART2,115200,timeout=1000, read_buf_len=4096)
-nic=network.ESP8285(uart)
-nic.connect(SSID, PASSWORD)
+class Tracker:
+    def __init__(self, ):
+        self.initlized = False
+        self.position = [0, 0, 0, 0]
+        self.velocity = [0, 0, 0, 0]
 
-addr = (MASTER_IP, 3456)
-sock = socket.socket()
-sock.connect(addr)
-sock.settimeout(5)
+    def predict(self):
+        box = [0, 0, 0, 0]
+        for i in range(4):
+            box[i] = self.position[i] + self.velocity[i]
+        return box
 
-sock.send(b'hello world!')
-sock.close()
+    def update(self, rect):
+        for i in range(4):
+            v_tmp = rect[i] - self.position[i]
+            self.position[i] = self.position[i] + self.velocity[i]
+            self.position[i] = 0.5*self.position[i] + 0.5*rect[i]
+            self.velocity[i] = 0.5*self.v_tmp + 0.5*self.velocity[i]
+
+
+############################
+# Init lcd
+############################
 
 lcd.init()
+
+
+############################
+# Network
+############################
+lcd.draw_string(0, 18*0, "Mouse Tracker", FONT_COLOT, lcd.BLACK)
+lcd.draw_string(0, 18*1, "Name: "+DEVICE_NAME, FONT_COLOT, lcd.BLACK)
+
+try:
+    fm.register(board_info.WIFI_RX,fm.fpioa.UART2_TX)
+    fm.register(board_info.WIFI_TX,fm.fpioa.UART2_RX)
+    uart = machine.UART(machine.UART.UART2,115200,timeout=2000, read_buf_len=4096)
+    lcd.draw_string(0, 18*2, "Init WiFi ...", FONT_COLOT, lcd.BLACK)
+    nic=network.ESP8285(uart)
+    lcd.draw_string(0, 18*2, "Connecting to WiFi: "+SSID, FONT_COLOT, lcd.BLACK)
+    nic.connect(SSID, PASSWORD)
+except KeyError as e:
+    lcd.draw_string(0, 18*3, str(e), FONT_COLOT, lcd.BLACK)
+    sys.exit()
+except OSError as e:
+    lcd.draw_string(0, 18*3, str(e), FONT_COLOT, lcd.BLACK)
+    sys.exit()
+else:
+    lcd.draw_string(0, 18*3, "WiFi Connected", FONT_COLOT, lcd.BLACK)
+
+
+
+############################
+# Socket
+############################
+
+lcd.draw_string(0, 18*4, "Connecting to Master: "+MASTER_IP, FONT_COLOT, lcd.BLACK)
+try:
+    addr = (MASTER_IP, 3456)
+    sock = socket.socket()
+    sock.connect(addr)
+    sock.settimeout(5)
+
+    sock.send(bytes(DEVICE_NAME, "ascii"))
+except KeyError as e:
+    lcd.draw_string(0, 18*5, "Failed: "+str(e), FONT_COLOT, lcd.BLACK)
+    sys.exit()
+except OSError as e:
+    lcd.draw_string(0, 18*5, "Failed: "+str(e), FONT_COLOT, lcd.BLACK)
+    sys.exit()
+else:
+    lcd.draw_string(0, 18*5, "Connected to Master", FONT_COLOT, lcd.BLACK)
+
+
+############################
+# Init camera
+############################
+
+lcd.draw_string(0, 18*6, "Initializing Camera...", FONT_COLOT, lcd.BLACK)
+
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
 sensor.set_hmirror(False)
 
 sensor.run(1)
+
+
+############################
+# Init yolo
+############################
+lcd.draw_string(0, 18*7, "Loading model...", FONT_COLOT, lcd.BLACK)
 task = kpu.load("/sd/yolo.kmodel")
 anchor = (0.750, 0.875, 2.875, 2.500, 2.625, 4.625, 5.625, 2.125, 4.625, 3.625)
 a = kpu.init_yolo2(task, 0.5, 0.3, 5, anchor)
+
+lcd.draw_string(0, 18*8, "Done!", FONT_COLOT, lcd.BLACK)
+
+############################
+# Main loop
+############################
+
+info = b""
 while(True):
     img = sensor.snapshot()
     code = kpu.run_yolo2(task, img)
+    info = b""
     if code:
         for i in code:
             print(i)
+            x, y, w, h = i.rect()
+            x += w//2
+            y += h//2
+            x = clip(x, 0, 255)
+            info += i.index().to_bytes(1, 0)
+            info += x.to_bytes(1, 0)
+            info += y.to_bytes(1, 0)
             a = img.draw_rectangle(i.rect())
+    sock.send(info)
     a = lcd.display(img)
 a = kpu.deinit(task)
+
+sock.close()
--- NPU/test_socket.py
+++ NPU/test_socket.py
@@ -1,13 +1,59 @@
-import socket
-ip_port = ('0.0.0.0',3456)
-sk = socket.socket()
-sk.bind(ip_port)
-sk.listen(5)
+'''
+    Simple socket server using threads
+'''
 
-while True:
-    print ('server waiting...')
-    conn,addr = sk.accept()
-    client_data = conn.recv(1024)
-    print (str(client_data,'utf8'))
-    conn.sendall(b'hello')
+import socket
+import sys
+from threading import *
+
+import numpy as np
+
+HOST = '0.0.0.0'    # Symbolic name meaning all available interfaces
+PORT = 3456    # Arbitrary non-privileged port
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print('Socket created')
+
+#Bind socket to local host and port
+s.bind((HOST, PORT))
+    
+print('Socket bind complete')
+
+#Start listening on socket
+s.listen(10)
+print('Socket now listening')
+
+#Function for handling connections. This will be used to create threads
+def clientthread(conn):
+    #Sending message to connected client
+    # conn.send('Welcome to the server. Type something and hit enter\n') #send only takes string
+    
+    #infinite loop so that function do not terminate and thread do not end.
+    name = conn.recv(10240)
+    print(f"Name: {name}")
+    while True:
+        
+        #Receiving from client
+        data = conn.recv(10240)
+        if not data:
+            break
+        data = np.frombuffer(data, dtype=np.uint8).reshape(-1, 3)
+        print(data)
+    
+        # conn.sendall(reply)
+    
+    #came out of loop
     conn.close()
+
+try:
+    #now keep talking with the client
+    while 1:
+        #wait to accept a connection - blocking call
+        conn, addr = s.accept()
+        print('Connected with ' + addr[0] + ':' + str(addr[1]))
+        
+        #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+        t = Thread(target=clientthread, args=(conn,), daemon=True)
+        t.start()
+except KeyboardInterrupt:
+    s.close()
